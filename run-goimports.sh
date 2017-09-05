6b1b9e884fb0292e8cf9c3548c541aa252d4f956
Sebastian Roll <sebastian.roll@webstep.no>
2017-09-05 11:56:04 +0200

    Fix up goimports bash

--- run-goimports.sh
+++ run-goimports.sh
@@ -1,28 +1,9 @@
 #!/usr/bin/env bash
+#
+# Capture and print stdout, since gofmt doesn't use proper exit codes
+#
+set -e
 
-# Redirect output to stderr.
-exec 1>&2
-
-# list staged go files
-# ignore trees starting with "_", like the go tool
-lsgo() {
-	git ls-files|egrep '.*[.]go$'|fgrep -v /_
-}
-
-# get the contents of $1 from the staging area
-# (the staging area is the stuff that will be committed)
-catstaged() {
-	git cat-file -p :$1
-}
-
-check() {
-	if test -n "`goimports -d <(catstaged $1)`"
-	then echo error: goimports $1 && return 1
-	fi
-}
-
-ok=true
-for file in `lsgo`
-do check $file || ok=false
-done
-test $ok = true
+exec 5>&1
+output="$(goimports -l -w "$@" | tee /dev/fd/5)"
+[[ -z "$output" ]]
